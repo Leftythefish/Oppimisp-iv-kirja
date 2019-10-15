6cece3dad0c0f075946192e1e515d7c44c29764f
Ria Ahlström <ria.ahlstrom@gmail.com>
2019-10-15 17:45:20 +0300

    editing html

---
+++ Oppimispäiväkirja_TEST/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Oppimispäiväkirja_TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UnitOfWork_StateUnderTest_ExpectedBehavior()
+        {
+        }
+
+        [TestMethod]
+        public void TopicHandler_StateUnderTest_ExpectedBehavior()
+        {
+        }
+
+    }
+}
